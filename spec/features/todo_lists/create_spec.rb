439550fe0fefc95c93b03cea64c574d7713a25af
Santrece Ross <mssross2@yahoo.com>
2014-06-05 17:03:14 -0500

    add more validations for description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -39,15 +39,15 @@ describe "Creating todo_lists" do
     click_link "New Todo list"
     expect(page).to have_content("New todo_list")
 
-    fill_in "Title", with: "Hi"
-    fill_in "Description", with: "This is what I'm doing today"
+    fill_in "Title", with: "Grocery list"
+    fill_in "Description", with: ""
     click_button "Create Todo list"
 
     expect(page).to have_content("error") 
     expect(TodoList.count).to eq(0)
 
     visit "/todo_lists"
-    expect(page).to_not have_content("This is what I'm doing today") 
+    expect(page).to_not have_content("Grocery list") 
   end  
 
   it "displays an error when the todo list has no description with less than 3 characters" do
@@ -67,4 +67,23 @@ describe "Creating todo_lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("This is what I'm doing today") 
   end  
+
+  it "displays an error when the todo list has no description with less than 5 characters" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Grocery list"
+    fill_in "Description", with: "none"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error") 
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("none") 
+  end  
+
 end
