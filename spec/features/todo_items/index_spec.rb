98f844f1f2875f1e27b89ca024a0578e4cdb7139
Santrece Ross <mssross2@yahoo.com>
2014-06-19 23:48:57 -0500

    todo list helpers module

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -4,13 +4,7 @@ describe "Viewing todo items" do
   let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.")}
 
 
- def visit_todo_list(list)
-   
-    visit "/todo_lists"
-    within "#todo_list_#{todo_list.id}" do
-      click_link "List Items"
-    end  
- end  
+ 
 
   it "displays the title of the todo list" do
     within("hi") do
